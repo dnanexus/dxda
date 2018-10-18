587f1235357aed12f622ad6894539f6e02245701
Geet Duggal <geetduggal@gmail.com>
2018-10-18 11:27:59 -0700

    Better help

--- cmd/dx-download-agent.go
+++ cmd/dx-download-agent.go
@@ -15,16 +15,23 @@ type downloadCmd struct {
 	maxThreads int
 }
 
+const downloadUsage = "dx-download-agent download [-max_threads N] <manifest.json.bz2>"
+
 func (*downloadCmd) Name() string     { return "download" }
 func (*downloadCmd) Synopsis() string { return "Download files in a manifest" }
 func (*downloadCmd) Usage() string {
-	return `download [-max_threads] manifest.json.bz2`
+	return downloadUsage
 }
 func (p *downloadCmd) SetFlags(f *flag.FlagSet) {
 	f.IntVar(&p.maxThreads, "max_threads", 8, "Maximum # of threads to use when downloading files")
 }
 
 func (p *downloadCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
+	// TODO: Is there a generic way to do this using subcommands?
+	if len(f.Args()) == 0 {
+		fmt.Println(downloadUsage)
+		os.Exit(1)
+	}
 	fname := f.Args()[0]
 	token, method := dxda.GetToken()
 	fmt.Printf("Obtained token using %s\n", method)
@@ -44,12 +51,20 @@ type progressCmd struct {
 
 func (*progressCmd) Name() string     { return "progress" }
 func (*progressCmd) Synopsis() string { return "Download files in a manifest" }
+
+const progressUsage = "dx-download-agent progress <manifest.json.bz2>"
+
 func (*progressCmd) Usage() string {
-	return `progress manifest.json.bz2`
+	return progressUsage
 }
 func (p *progressCmd) SetFlags(f *flag.FlagSet) {
 }
 func (p *progressCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
+	// TODO: Is there a generic way to do this using subcommands?
+	if len(f.Args()) == 0 {
+		fmt.Println(progressUsage)
+		os.Exit(1)
+	}
 	fname := f.Args()[0]
 	fmt.Println(dxda.DownloadProgress(fname))
 	return subcommands.ExitSuccess
@@ -62,6 +77,11 @@ func main() {
 	subcommands.Register(subcommands.CommandsCommand(), "")
 	subcommands.Register(&downloadCmd{}, "")
 	subcommands.Register(&progressCmd{}, "")
+	// TODO: modify this to use individual subcommand help
+	if len(os.Args) == 1 {
+		fmt.Printf("Usage:\n  For progress:\n  $ %s\n\n  For downloading:\n  $ %s\n", progressUsage, downloadUsage)
+		os.Exit(1)
+	}
 
 	flag.Parse()
 	ctx := context.Background()
