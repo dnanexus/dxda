fdf8dfc99be49567456d522cb47ad50e300b3911
Geet Duggal <geetduggal@gmail.com>
2018-11-13 16:46:11 -0800

    Adding create_release script

---
+++ create_release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -ex
+
+TAG=$1
+
+docker pull dnanexus/dxda:$TAG
+
+VERSION=$2
+mkdir -p builds/
+docker run --entrypoint='' dnanexus/dxda:$TAG cat /go/bin/dx-download-agent > builds/dx-download-agent-linux
+docker run --entrypoint='' dnanexus/dxda:$TAG cat /dx-download-agent-osx > builds/dx-download-agent-osx
+
+docker tag dnanexus/dxda:$TAG dnanexus/dxda:$VERSION
+docker push dnanexus/dxda:$VERSION
+
+echo "SUCCESS. Builds placed in build/ directory"
