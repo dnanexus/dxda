02e29a25f31b023e00198c6fc46cb34882c48edd
Geet Duggal <geetduggal@Geets-MacBook-Pro.local>
2018-12-11 21:48:56 -0800

    Basic listing of manifest

---
+++ scripts/ls_manifest.py
@@ -0,0 +1,37 @@
+import argparse
+from pprint import pprint
+import json
+import bz2
+import re
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Filter a manifest file by a regular expression on file name')
+    parser.add_argument('manifest_file', help='BZIP2-compressed JSON manifest')
+    parser.add_argument('regex', nargs='?', default=".*", help="Regular expression")
+    parser.add_argument("--long", "-l", action='store_true', help="Long description for each file")
+    args = parser.parse_args()
+    
+    with open(args.manifest_file) as mf:
+        manifest = json.loads(bz2.decompress(mf.read()))
+    new_manifest = {}
+
+    def fsize(f):
+        return sum([meta['size'] for id, meta in f['parts'].items()])
+
+    for project, file_list in manifest.items():
+        matches = [f for f in file_list if re.match(args.regex, f['name'])]
+        total_size = sum([fsize(f) for f in matches])
+        for f in sorted(matches, key=lambda x: x['name']):
+            lsline = "{}{}".format(f['folder'], f['name'])
+            if args.long:
+                lsline = lsline+"\t{}".format(fsize(f))
+            print(lsline)
+        print("")
+        print("{} files total {} MB".format(len(matches), total_size/(1024*1024)))
+
+
+
+
+if __name__ == "__main__":
+    main()
