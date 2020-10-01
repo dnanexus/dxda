ad780233f8e846b81b0f4b8b57aa1c03988749d4
Kurt Jensen <kjensen@dnanexus.com>
2020-10-01 17:15:46 +0000

    Bump version to v0.5.3

--- util.go
+++ util.go
@@ -28,7 +28,7 @@ const (
 
 	// Extracted automatically with a shell script, so keep the format:
 	// version = XXXX
-	Version = "v0.5.2"
+	Version = "v0.5.3"
 )
 
 // Configuration options for the download agent
