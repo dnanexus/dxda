5d2576c72417f4016e25457ff9b2a7bc79c7e908
Austin Locke <alocke@dnanexus.com>
2024-04-04 12:15:31 -0500

    Update describe to only add symlink for files without parts

--- dx_describe.go
+++ dx_describe.go
@@ -150,7 +150,7 @@ func submit(
 		// If this is a symlink, create structure with
 		// all the relevant information.
 		var symlink *DXSymlink = nil
-		if descRaw.Drive != nil {
+		if descRaw.Parts == nil && descRaw.Drive != nil {
 			symlink = &DXSymlink{
 				MD5:   *descRaw.MD5,
 				Drive: *descRaw.Drive,
